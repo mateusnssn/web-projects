b32c9c73906a60c81eec46efd5cba1587736d365
Mateus Nunes <mateusnss@protonmail.ch>
2021-05-21 17:42:06 -0300

    Update main.js

--- pomodoro/main.js
+++ pomodoro/main.js
@@ -15,7 +15,7 @@ function start(){
         timer();
         if(minute == 0 && second == 0){
             pause();
-            relay();
+            relay(0, "");
         }
         
     }, time);
@@ -59,71 +59,141 @@ const step_data = {
 };
 let counter = 0;
 
-
 // tags
 let pomodoro_tag = document.getElementById("pomodoro_tag");
 let short_break_tag = document.getElementById("short_break_tag");
 let long_break_tag = document.getElementById("long_break_tag");
 
 
-function relay(){
+function relay(command_type, c){
+
+    // command_type:    1 --> Modo troca manual;
+    //                  0 --> Modo de troca automática.
     
-    if(step == step_data.pomodoro.type && counter == 3){
-        step = step_data.long_break.type;
-
-        minute = step_data.long_break.time;
-        second = 0;
-        counter = 0;
-
-        pomodoro_tag.classList.remove("select");
-        long_break_tag.classList.add("select");
-
-        console.log(1)
-
-        new Audio("./assets/alarm.mp3").play();
-        return;
-
-    }if(step == step_data.pomodoro.type && counter < 3){
-        step = step_data.short_break.type;
-
-        minute = step_data.short_break.time;
-        second = 0;
-        counter++;
-
-        pomodoro_tag.classList.remove("select");
-        short_break_tag.classList.add("select");
-        console.log(2)
-        new Audio("./assets/alarm.mp3").play();
-
-        return;
+    if(command_type == 0){
+        if(step == step_data.pomodoro.type && counter == 3){
+            step = step_data.long_break.type;
+    
+            minute = step_data.long_break.time;
+            second = 0;
+            counter = 0;
+    
+            pomodoro_tag.classList.remove("select");
+            long_break_tag.classList.add("select");
+    
+            console.log(1)
+    
+            new Audio("./assets/alarm.mp3").play();
+            return;
+    
+        }if(step == step_data.pomodoro.type && counter < 3){
+            step = step_data.short_break.type;
+    
+            minute = step_data.short_break.time;
+            second = 0;
+            counter++;
+    
+            pomodoro_tag.classList.remove("select");
+            short_break_tag.classList.add("select");
+            console.log(2)
+            new Audio("./assets/alarm.mp3").play();
+    
+            return;
+        }
+    
+        if(step == step_data.short_break.type){
+            step = step_data.pomodoro.type;
+    
+            minute = step_data.pomodoro.time;
+            second = 0;
+    
+            short_break_tag.classList.remove("select");
+            pomodoro_tag.classList.add("select");
+    
+            console.log(3)
+            new Audio("./assets/alarm.mp3").play();
+            return;
+    
+        }if(step == step_data.long_break.type){
+            step = step_data.pomodoro.type;
+    
+            minute = step_data.pomodoro.time;
+            second = 0;
+    
+            long_break_tag.classList.remove("select");
+            pomodoro_tag.classList.add("select");
+    
+            console.log(4)
+            new Audio("./assets/alarm.mp3").play();
+            return;
+        }
     }
 
-    if(step == step_data.short_break.type){
-        step = step_data.pomodoro.type;
+    if(command_type == 1){
+        
+        if(c == "pomodoro"){
 
-        minute = step_data.pomodoro.time;
-        second = 0;
 
-        short_break_tag.classList.remove("select");
-        pomodoro_tag.classList.add("select");
+            step = step_data.pomodoro.type;
+    
+            minute = step_data.pomodoro.time;
+            second = 0;
+            counter = 0;
+    
+            pomodoro_tag.classList.add("select");
+            long_break_tag.classList.remove("select");
+            short_break_tag.classList.remove("select");
+            return;
 
-        console.log(3)
-        new Audio("./assets/alarm.mp3").play();
-        return;
+        }if(c == "short_break"){
 
-    }if(step == step_data.long_break.type){
-        step = step_data.pomodoro.type;
+            step = step_data.short_break.type;
+    
+            minute = step_data.short_break.time;
+            second = 0;
+            counter = 0;
+    
+            pomodoro_tag.classList.remove("select");
+            long_break_tag.classList.remove("select");
+            short_break_tag.classList.add("select");
+            return;
 
-        minute = step_data.pomodoro.time;
-        second = 0;
 
-        long_break_tag.classList.remove("select");
-        pomodoro_tag.classList.add("select");
 
-        console.log(4)
-        new Audio("./assets/alarm.mp3").play();
-        return;
+        }if(c == "long_break"){
+
+
+            step = step_data.long_break.type;
+    
+            minute = step_data.long_break.time;
+            second = 0;
+            counter = 0;
+    
+            pomodoro_tag.classList.remove("select");
+            long_break_tag.classList.add("select");
+            short_break_tag.classList.remove("select");
+            return;
+
+        }
+
+
     }
 
 
 }
+
+// relay
+
+function def_pomodoro(){
+    relay(1, "pomodoro");
+
+}
+
+function def_short_break(){
+    relay(1, "short_break");
+}
+
+function def_long_break(){
+    relay(1, "long_break");
+}
+
