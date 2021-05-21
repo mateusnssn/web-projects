1bcc4eaa443928fc66daa161149159ba20c8ed7b
Mateus Nunes <mateusnss@protonmail.ch>
2021-05-21 19:57:11 -0300

    Update main.js

--- pomodoro/main.js
+++ pomodoro/main.js
@@ -5,7 +5,6 @@ var second = 0;
 const time = 1000; // 1 segundo;
 var chronometer;
 
-
 let clock_tag = document.getElementById(`clock`);
 let controllerButton = document.getElementById(`controller`);
 
@@ -21,7 +20,6 @@ function start(){
     }, time);
     controllerButton.innerHTML = "Pause";
     controllerButton.setAttribute('onclick', 'pause()');
-
     clock_tag.classList.remove(`pause`);
     clock_tag.classList.add(`time`);
 }
@@ -30,7 +28,6 @@ function pause(){
     clearInterval(chronometer);
     controllerButton.innerHTML = "Start";
     controllerButton.setAttribute('onclick', 'start()');
-
     clock_tag.classList.remove(`time`);
     clock_tag.classList.add(`pause`);
 }
@@ -41,9 +38,7 @@ function timer(){
 
     if(second == -1){
         second = 59;
-        minute--;
-
-        
+        minute--;  
     }
 
     var format = `${(minute < 10 ? '0' + minute : minute)}:${(second < 10 ? '0' + second : second)}`;
@@ -59,145 +54,117 @@ const step_data = {
 };
 let counter = 0;
 
+
 // tags
 let pomodoro_tag = document.getElementById("pomodoro_tag");
 let short_break_tag = document.getElementById("short_break_tag");
 let long_break_tag = document.getElementById("long_break_tag");
 
 
+//relê automático
 function relay(command_type, c){
 
-    // command_type:    1 --> Modo troca manual;
-    //                  0 --> Modo de troca automática.
-    
+   /*
+    *   command_type:    1 --> Modo troca manual;
+    *                    0 --> Modo de troca automática.
+    */
+
     if(command_type == 0){
         if(step == step_data.pomodoro.type && counter == 3){
             step = step_data.long_break.type;
-    
             minute = step_data.long_break.time;
             second = 0;
             counter = 0;
-    
             pomodoro_tag.classList.remove("select");
             long_break_tag.classList.add("select");
-    
-            console.log(1)
-    
             new Audio("./assets/alarm.mp3").play();
             return;
     
         }if(step == step_data.pomodoro.type && counter < 3){
             step = step_data.short_break.type;
-    
             minute = step_data.short_break.time;
             second = 0;
             counter++;
-    
             pomodoro_tag.classList.remove("select");
             short_break_tag.classList.add("select");
-            console.log(2)
             new Audio("./assets/alarm.mp3").play();
-    
             return;
         }
     
         if(step == step_data.short_break.type){
             step = step_data.pomodoro.type;
-    
             minute = step_data.pomodoro.time;
             second = 0;
-    
             short_break_tag.classList.remove("select");
             pomodoro_tag.classList.add("select");
-    
-            console.log(3)
             new Audio("./assets/alarm.mp3").play();
             return;
     
         }if(step == step_data.long_break.type){
             step = step_data.pomodoro.type;
-    
             minute = step_data.pomodoro.time;
             second = 0;
-    
             long_break_tag.classList.remove("select");
             pomodoro_tag.classList.add("select");
-    
-            console.log(4)
             new Audio("./assets/alarm.mp3").play();
             return;
+
         }
     }
 
     if(command_type == 1){
         
         if(c == "pomodoro"){
-
-
             step = step_data.pomodoro.type;
-    
             minute = step_data.pomodoro.time;
             second = 0;
-
             pause();
-    
             pomodoro_tag.classList.add("select");
             long_break_tag.classList.remove("select");
             short_break_tag.classList.remove("select");
             return;
 
         }if(c == "short_break"){
-
             step = step_data.short_break.type;
-    
             minute = step_data.short_break.time;
             second = 0;
-
             pause();
-
-    
             pomodoro_tag.classList.remove("select");
             long_break_tag.classList.remove("select");
             short_break_tag.classList.add("select");
             return;
 
-
-
         }if(c == "long_break"){
-
-
             step = step_data.long_break.type;
-    
             minute = step_data.long_break.time;
             second = 0;
-
             pause();
-    
             pomodoro_tag.classList.remove("select");
             long_break_tag.classList.add("select");
             short_break_tag.classList.remove("select");
             return;
 
         }
-
-
     }
-
-
 }
 
-// relay
+// relê manual
+function update_time(){
+    var format = `${(minute < 10 ? '0' + minute : minute)}:${(second < 10 ? '0' + second : second)}`;
+    document.getElementById(`clock`).innerHTML = format;
+}
 
 function def_pomodoro(){
     relay(1, "pomodoro");
-
+    update_time();
 }
 
 function def_short_break(){
     relay(1, "short_break");
+    update_time();
 }
 
 function def_long_break(){
     relay(1, "long_break");
+    update_time();
 }
-
