0c7cf410f19ca4e5863fbe0e693a83f34bcc7e6c
Mateus Nunes <mateusnss@protonmail.ch>
2021-05-21 16:52:54 -0300

    Update main.js

--- pomodoro/main.js
+++ pomodoro/main.js
@@ -60,11 +60,63 @@ let counter = 0;
 
 
 // tags
-let pomodoro_tag = document.getElementsByName("pomodoro_tag");
+let pomodoro_tag = document.getElementById("pomodoro_tag");
 let short_break_tag = document.getElementById("short_break_tag");
 let long_break_tag = document.getElementById("long_break_tag");
 
 
 function relay(){
     
+    if(step == step_data.pomodoro.type && counter == 3){
+        step = step_data.long_break.type;
+
+        minute = step_data.long_break.time;
+        second = 0;
+        counter = 0;
+
+        pomodoro_tag.classList.remove("select");
+        long_break_tag.classList.add("select");
+
+        console.log(1)
+        return;
+
+    }if(step == step_data.pomodoro.type && counter < 3){
+        step = step_data.short_break.type;
+
+        minute = step_data.short_break.time;
+        second = 0;
+        counter++;
+
+        pomodoro_tag.classList.remove("select");
+        short_break_tag.classList.add("select");
+        console.log(2)
+        return;
+    }
+
+    if(step == step_data.short_break.type){
+        step = step_data.pomodoro.type;
+
+        minute = step_data.pomodoro.time;
+        second = 0;
+
+        short_break_tag.classList.remove("select");
+        pomodoro_tag.classList.add("select");
+
+        console.log(3)
+        return;
+
+    }if(step == step_data.long_break.type){
+        step = step_data.pomodoro.type;
+
+        minute = step_data.pomodoro.time;
+        second = 0;
+
+        long_break_tag.classList.remove("select");
+        pomodoro_tag.classList.add("select");
+
+        console.log(4)
+        return;
+    }
+
+
 }
